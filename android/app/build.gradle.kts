488dbd2de9451f8cabe4aa46b61f154b3d796164
rosdeb <rodebkoch455@gmail.com>
2025-12-06 20:18:09 +0600

    update marker

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -25,7 +25,8 @@ android {
         applicationId = "com.example.google"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 21
+       // minSdkVersion = flutter.minSdkVersion
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
